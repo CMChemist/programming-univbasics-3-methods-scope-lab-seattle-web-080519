87a1d9f74e8ee4a075c3673092d2e072f16e995c
Joseph Trettevik <josephtrettevik@gmail.com>
2019-07-26 19:15:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
-def mario
-other Castle!'  status = 'Thank You Mario! But Our Princess Is In An
+def mario  status = 'Thank You Mario! But Our Princess Is In An
   phrase = 'It\'s-a me, Mario!'
   puts phrase
 end
