a954f9201744eefdccf0b4973974b6ee625a675d
Joseph Trettevik <josephtrettevik@gmail.com>
2019-07-26 19:16:14 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
-def mario  status = 'Thank You Mario! But Our Princess Is In An
+def mario  
   phrase = 'It\'s-a me, Mario!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another'
   puts status
 end
